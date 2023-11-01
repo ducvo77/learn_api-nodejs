871a4b734bdc4c399d1530d4dc3cc557e3ef0693
Coder1603 <95237500+Coder1603@users.noreply.github.com>
2023-11-01 22:26:39 +0700

    config project

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
-import { Request, Response } from "express";
+import express, { Request, Response, Express } from 'express'
 
-const express = require("express");
+const app: Express = express()
 
-const app = express();
+const hostname: string = 'localhost'
+const port: number = 8080
 
-const hostname = "localhost";
-const port = 8080;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, hostname, () => {
-  console.log(`Running server at http://${hostname + ":" + port}/`);
-});
+  console.log(`Running server at http://${hostname + ':' + port}/`)
+})
